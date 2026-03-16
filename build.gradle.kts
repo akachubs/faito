c1862a15757398f9b0f43196edb5c8356fafb3cb
Your Name <your-github-email@example.com>
2026-03-16 20:06:34 +0530

    yo

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,9 +12,8 @@ version = "0.0.1-SNAPSHOT"
 description = "springboot"
 
 java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(17)
-	}
+	sourceCompatibility = JavaVersion.VERSION_17
+	targetCompatibility = JavaVersion.VERSION_17
 }
 
 repositories {
